4660f4e5144a3fcca09406d954da362b65e8662a
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-08 20:57:55 +0700

    настроен роутинг для запросов с фильмами

--- src/movieCatalog/api/__init__.py
+++ src/movieCatalog/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .genres import router as genres_router
+from .movies import router as movies_router
 
 router = APIRouter()
 router.include_router(genres_router)
+router.include_router(movies_router)
