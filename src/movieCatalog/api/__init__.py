7ba0e9db732b42c443af5eef0176667725786de8
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-10 15:12:22 +0700

    Настроен роутинг для запросов, связанных с ревью

--- src/movieCatalog/api/__init__.py
+++ src/movieCatalog/api/__init__.py
@@ -4,9 +4,11 @@ from .genres import router as genres_router
 from .movies import router as movies_router
 from .auth import router as auth_router
 from .favoriteMovies import router as favorite_movies_router
+from .review import router as review_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(genres_router)
 router.include_router(movies_router)
 router.include_router(favorite_movies_router)
+router.include_router(review_router)
