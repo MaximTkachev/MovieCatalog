e7a4a1662d63d5e467926963607bff17ce5c2394
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-10 12:19:40 +0700

    Настроен роутинг для запросов с коллекцией любимых фильмов

--- src/movieCatalog/api/__init__.py
+++ src/movieCatalog/api/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from .genres import router as genres_router
 from .movies import router as movies_router
 from .auth import router as auth_router
+from .favoriteMovies import router as favorite_movies_router
 
 router = APIRouter()
+router.include_router(auth_router)
 router.include_router(genres_router)
 router.include_router(movies_router)
-router.include_router(auth_router)
+router.include_router(favorite_movies_router)
