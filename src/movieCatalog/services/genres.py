61116257a4d7ccff7c114da57b9ef8cbc554fdcb
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-04-16 14:40:40 +0700

    Добавлена проверка на совпадающие названия жанров при создании нового или редактировании существующего.

--- src/movieCatalog/services/genres.py
+++ src/movieCatalog/services/genres.py
@@ -14,6 +14,16 @@ class GenreService:
         self.session = session
 
     def create(self, genre_data: GenreCreate) -> tables.Genre:
+        genre_check = (
+            self.session
+                .query(tables.Genre)
+                .filter_by(name=genre_data.name)
+                .first()
+        )
+        if genre_check:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Genre with the same name already exists")
+
         genre = tables.Genre(
             **genre_data.dict()
         )
@@ -24,17 +34,17 @@ class GenreService:
     def get_genres(self) -> List[tables.Genre]:
         genres = (
             self.session
-            .query(tables.Genre)
-            .all()
+                .query(tables.Genre)
+                .all()
         )
         return genres
 
     def get_genre(self, genre_id: int) -> tables.Genre:
         genre = (
             self.session
-            .query(tables.Genre)
-            .filter_by(id=genre_id)
-            .first()
+                .query(tables.Genre)
+                .filter_by(id=genre_id)
+                .first()
         )
 
         if not genre:
@@ -47,6 +57,17 @@ class GenreService:
         self.session.commit()
 
     def edit_genre(self, genre_id: int, genre_data: GenreEdit):
+        genre_check = (
+            self.session
+                .query(tables.Genre)
+                .filter(tables.Genre.id != genre_id, tables.Genre.name == genre_data.name)
+                .all()
+        )
+
+        if genre_check:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Genre with the same name already exists")
+
         genre = self.get_genre(genre_id=genre_id)
         for field, value in genre_data:
             if value is not None:
